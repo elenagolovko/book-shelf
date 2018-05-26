7723b34bae8a46ed25321a70bb523630fdbe14e2
elenagolovko <airdragon220@gmail.com>
2018-05-26 16:10:43 +0300

    my first web app

---
+++ book-shelf/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+// import { DxButtonModule } from 'devextreme-angular';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
